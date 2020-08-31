3c4ab788b043eee3e3e7ed2ea440dcd010f2b7a9
Piyush Yadav <PiyushTheGoku@gamil.com>
2020-09-01 03:09:58 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '313483149988974',
+        clientSecret: 'b45f0585b296848064b244cdc14fa60b'
+    }
 }
